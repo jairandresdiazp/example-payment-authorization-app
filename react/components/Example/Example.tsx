4c8853ceee943900f2af43b0f1e55722d688786e
Jair Andres Diaz Puentes <jairdiaz@blacksip.com>
2022-01-28 20:45:19 -0500

    fix: ajuste para que funcione con typescript

--- react/components/Example/Example.tsx
+++ react/components/Example/Example.tsx
@@ -1,111 +1,123 @@
-import React, { Fragment, useState, useEffect, useRef } from 'react'
-import $ from "jquery";
-import styles from './styles.css'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react/button-has-type */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable no-console */
+import React, { useState, useEffect, useRef } from 'react';
+import styles from './styles.css';
+
+declare const $: any;
 
 const Example: StorefrontFunctionComponent = (props: any) => {
-  const [state, setState] = useState({
-    scriptLoaded: false,
-    loading: false,
-  })
+    const [state, setState] = useState({
+        scriptLoaded: false,
+        loading: false,
+    });
 
-  const { scriptLoaded, loading } = state
+    const { scriptLoaded, loading } = state;
 
-  const divContainer = useRef<any>(null)
+    const divContainer = useRef<any>(null);
 
-  useEffect(() => {
-    injectScript(
-      'google-recaptcha-v2',
-      'https://recaptcha.net/recaptcha/api.js?render=explicit',
-      handleOnLoad
-    )
-    $(window).trigger('removePaymentLoading.vtex')
-  }, [])
+    const injectScript = (
+        id: string,
+        src: string,
+        onLoad: ((this: GlobalEventHandlers, ev: Event) => any) | null
+    ) => {
+        if (document.getElementById(id)) {
+            return;
+        }
 
-  const respondTransaction = (status: boolean) => {
-    $(window).trigger('transactionValidation.vtex', [status])
-  }
+        const head = document.getElementsByTagName('head')[0];
 
-  const handleOnLoad = () => {
-    setState({ ...state, scriptLoaded: true })
-    window?.grecaptcha.ready(() => {
-      window?.grecaptcha.render(divContainer.current, {
-        sitekey: '------>REPATCHA_V2_SITE_KEY<------', //Replace with site key
-        theme: 'dark',
-        callback: onVerify,
-      })
-    })
-  }
+        const js = document.createElement('script');
+        js.id = id;
+        js.src = src;
+        js.async = true;
+        js.defer = true;
+        js.onload = onLoad;
 
-  const onVerify = (_e: any) => {
-    const parsedPayload = JSON.parse(props.appPayload)
-    setState({ ...state, loading: true })
+        head.appendChild(js);
+    };
 
-    fetch(parsedPayload.approvePaymentUrl).then(() => {
-      respondTransaction(true)
-    })
-  }
+    const respondTransaction = (status: boolean) => {
+        $(window).trigger('transactionValidation.vtex', [status]);
+    };
 
-  const cancelTransaction = () => {
-    const parsedPayload = JSON.parse(props.appPayload)
-    setState({ ...state, loading: true })
+    const onVerify = () => {
+        const parsedPayload = JSON.parse(props.appPayload);
+        setState({ ...state, loading: true });
 
-    fetch(parsedPayload.denyPaymentUrl).then(() => {
-      respondTransaction(false)
-    })
-  }
+        fetch(parsedPayload.approvePaymentUrl).then(() => {
+            respondTransaction(true);
+        });
+    };
 
-  const confirmTransation = () => {
-    const parsedPayload = JSON.parse(props.appPayload)
-    setState({ ...state, loading: true })
+    const handleOnLoad = () => {
+        setState({ ...state, scriptLoaded: true });
+        window?.grecaptcha.ready(() => {
+            window?.grecaptcha.render(divContainer.current, {
+                sitekey: '------>REPATCHA_V2_SITE_KEY<------', // Replace with site key
+                theme: 'dark',
+                callback: onVerify,
+            });
+        });
+    };
 
-    fetch(parsedPayload.approvePaymentUrl).then(() => {
-      respondTransaction(true)
-    })
-  }
+    useEffect(() => {
+        injectScript(
+            'google-recaptcha-v2',
+            'https://recaptcha.net/recaptcha/api.js?render=explicit',
+            handleOnLoad
+        );
+        $(window).trigger('removePaymentLoading.vtex');
+    }, []);
 
-  const injectScript = (id: string, src: string, onLoad: ((this: GlobalEventHandlers, ev: Event) => any) | null) => {
-    if (document.getElementById(id)) {
-      return
-    }
+    const cancelTransaction = () => {
+        const parsedPayload = JSON.parse(props.appPayload);
+        setState({ ...state, loading: true });
 
-    const head = document.getElementsByTagName('head')[0]
+        fetch(parsedPayload.denyPaymentUrl).then(() => {
+            respondTransaction(false);
+        });
+    };
 
-    const js = document.createElement('script')
-    js.id = id
-    js.src = src
-    js.async = true
-    js.defer = true
-    js.onload = onLoad
+    const confirmTransation = () => {
+        const parsedPayload = JSON.parse(props.appPayload);
+        setState({ ...state, loading: true });
 
-    head.appendChild(js)
-  }
+        fetch(parsedPayload.approvePaymentUrl).then(() => {
+            respondTransaction(true);
+        });
+    };
 
-  return (
-    <div className={styles.wrapper}>
-      {scriptLoaded && !loading ? (
-        <Fragment>
-          <div className="g-recaptcha" ref={divContainer}></div>
-          <button
-            id="payment-app-confirm"
-            className={styles.buttonSuccess}
-            onClick={confirmTransation}>
-            Confirmar
-          </button>
-        </Fragment>
-      ) : (
-        <h2>Carregando...</h2>
-      )}
+    return (
+        <div className={styles.wrapper}>
+            {scriptLoaded && !loading ? (
+                <>
+                    <div className="g-recaptcha" ref={divContainer} />
+                    <button
+                        id="payment-app-confirm"
+                        className={styles.buttonSuccess}
+                        onClick={confirmTransation}
+                    >
+                        Confirmar
+                    </button>
+                </>
+            ) : (
+                <h2>Carregando...</h2>
+            )}
 
-      {!loading && (
-        <button
-          id="payment-app-cancel"
-          className={styles.buttonDanger}
-          onClick={cancelTransaction}>
-          Cancelar
-        </button>
-      )}
-    </div>
-  )
-}
+            {!loading && (
+                <button
+                    id="payment-app-cancel"
+                    className={styles.buttonDanger}
+                    onClick={cancelTransaction}
+                >
+                    Cancelar
+                </button>
+            )}
+        </div>
+    );
+};
 
-export default Example
+export default Example;
